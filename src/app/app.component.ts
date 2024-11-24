6b61de43259040aca632a0877ae84b7ba113e4c7
ThiagoFranca <francasantos.t@gmail.com>
2024-11-23 20:33:53 -1200

    Primeiro commit todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-first-app';
+  users = DUMMY_USERS;
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true, 
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+   @Input() avatar!: string;
+   @Input() name!: string;
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelecteUser(){
+    
+  }
+
+}
